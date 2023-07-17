4900c49b59763b031cd142e72fb7baae5e3e3f2f
Ravi <ravirrajpatil@gmailcom>
2023-07-18 00:00:07 +0530

    added responsiveness to the website

---
+++ index.js
@@ -0,0 +1,9 @@
+newFunction();
+function newFunction() {
+  const toggleButton = document.getElementById("nav-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
